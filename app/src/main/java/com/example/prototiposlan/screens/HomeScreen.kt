14cdf56bd12b76f8f33f685174af0bb70a40d895
Fabian <fevr.developer@gmail.com>
2023-09-01 21:14:18 -0500

    1/9/23

--- app/src/main/java/com/example/prototiposlan/screens/HomeScreen.kt
+++ app/src/main/java/com/example/prototiposlan/screens/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.example.prototiposlan.screens
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
@@ -13,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -140,6 +142,12 @@ fun DrawerItem(item: LatMenuScreens, navController: NavController) {
 @Composable
 fun HomeInfo() {
 
+}
+
+@Preview
+@Composable
+fun DaySchedule(){
+    LazyColumn(content = {})
 }
 
 
